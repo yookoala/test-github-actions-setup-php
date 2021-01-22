6284a50bf2a314234be42d6e9a589411334ef68b
Koala Yeung <koalay@gmail.com>
2021-01-23 01:14:15 +0800

    wip

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
  * Attempt to reproduce setup-php issue with displaying error.
  */
 
-ini_set('display_errors', 1);
+#ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(-1);
 
